5d05edd886607f584c53760992bfe3c355e0a6b6
raintimewalking <91025579+raintimewalking@users.noreply.github.com>
2021-09-26 00:17:15 +0800

    test

--- src/main/java/com/example/demo/config/LoginConfig.java
+++ src/main/java/com/example/demo/config/LoginConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class LoginConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        System.out.println("view=========");
+        System.out.println("view============");
         registry.addViewController("/").setViewName("login");
         registry.addViewController("/index.html").setViewName("login");
     }
